f400bace79e64ffe34a867bc4ac06aedb29b608a
ASxCRO <antonio.suups@gmail.com>
2025-05-26 16:52:53 +0200

    config & vmBase

---
+++ Models/AppConfiguration.cs
@@ -0,0 +1,5 @@
+﻿namespace PrinterJobInterceptor.Models;
+
+public class AppConfiguration
+{
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace PrinterJobInterceptor.ViewModels;
+
+public class ViewModelBase : INotifyPropertyChanged
+{
+    public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
