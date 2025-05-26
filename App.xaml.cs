3ff42fbecc7d296bf2c03c6c51980d482fb92aa3
ASxCRO <antonio.suups@gmail.com>
2025-05-26 16:39:33 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PrinterJobInterceptor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
