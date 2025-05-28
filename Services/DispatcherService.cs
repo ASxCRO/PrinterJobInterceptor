c5aa8e3d525325fbc708af5b6f341eac0f91f50a
ASxCRO <antonio.suups@gmail.com>
2025-05-28 19:00:46 +0200

    dispatcher service

---
+++ Services/DispatcherService.cs
@@ -0,0 +1,7 @@
+﻿using PrinterJobInterceptor.Services.Interfaces;
+
+namespace PrinterJobInterceptor.Services;
+public class DispatcherService : IDispatcherService
+{
+    public void Invoke(Action action) => App.Current.Dispatcher.Invoke(action);
+}
---
+++ Services/Interfaces/IDispatcherService.cs
@@ -0,0 +1,5 @@
+﻿namespace PrinterJobInterceptor.Services.Interfaces;
+public interface IDispatcherService
+{
+    void Invoke(Action action);
+}
